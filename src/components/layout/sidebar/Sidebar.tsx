3fdea17a0228c489c3706457399baf56adb081e1
TechDanil <danilkomarov243242@gmail.com>
2024-01-03 21:58:46 +0500

    added links to sidebar and styles to it

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,3 +1,31 @@
+import { CHATS, FRIENDS, PHONE, SETTINGS } from '@/configs/index.config'
+import { MessagesSquare, Phone, Sun, Users2 } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from './Sidebar.module.scss'
+
 export function Sidebar() {
-	return <div>Sidebar</div>
+	return (
+		<aside className={styles.sidebar}>
+			<Image src='/logo.svg' priority alt='logo' width={40} height={40} />
+			<div>
+				<Link href={FRIENDS}>
+					<Users2 />
+				</Link>
+
+				<Link href={PHONE}>
+					<Phone />
+				</Link>
+
+				<Link href={CHATS}>
+					<MessagesSquare />
+				</Link>
+
+				<Link href={SETTINGS}>
+					<MessagesSquare />
+				</Link>
+			</div>
+			<Sun />
+		</aside>
+	)
 }
---
+++ src/configs/index.config.ts
@@ -0,0 +1,6 @@
+//ROUTES
+
+export const FRIENDS = '/friends'
+export const PHONE = '/call'
+export const CHATS = '/chats'
+export const SETTINGS = 'settings'
